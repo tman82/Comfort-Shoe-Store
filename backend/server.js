b0b751261079aec8d70d3579fb9a7b750bf91fa6
Martrell Leonard <martrell3030@gmail.com>
2020-11-10 01:12:29 -0600

    adding shoe store app

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,21 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require('path')
 const shoeRouter = require('./shoe.route')
 const userRouter = require('./user.route')
 const emailRouter = require('./email.route')
 //const routes = express.Router()
 
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 
-mongoose.connect('mongodb+srv://tre1238:c4t0ugCOi411boy1@trecluster-e058a.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://tre1238:c4t0ugCOi411boy1@trecluster-e058a.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
 const connection = mongoose.connection
 
 connection.once('open', () => {
@@ -26,6 +27,14 @@ connection.once('open', () => {
 app.use('/', shoeRouter)
 app.use('/', userRouter)
 app.use('/', emailRouter)
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'))
+    })
+}
 
 
 app.listen(PORT, () => {
